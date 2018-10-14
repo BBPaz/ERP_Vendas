e8f0fb2fcc35077caf0476f2508326495532603f
Igoralves12 <igor.siviero1@gmail.com>
2018-10-13 23:30:29 -0300

    centro de custo + xml

---
+++ src/java/entidades/CentroCusto.java
@@ -0,0 +1,106 @@
+
+package entidades;
+
+public class CentroCusto {
+    private String CNPJ1 = "99999999999999"; // cnpj
+    private String rSocial = "ADS PANELAS"; //razao social
+    private String fantasia = "ADS";
+    private String longradouro = "Av. Reg. Feijó";
+    private int nro = 1295;
+    private String bairro = "Jardim Analia Franco";
+    private String municipio = "Sao paulo";
+    private String uf = "SP";
+    private String cep = "03576160";
+    private String fone = "948601713";
+    private String ie = "123456789"; // inscricao estadual
+
+    public String getCNPJ1() {
+        return CNPJ1;
+    }
+
+    public void setCNPJ1(String CNPJ1) {
+        this.CNPJ1 = CNPJ1;
+    }
+
+    public String getrSocial() {
+        return rSocial;
+    }
+
+    public void setrSocial(String rSocial) {
+        this.rSocial = rSocial;
+    }
+
+    public String getFantasia() {
+        return fantasia;
+    }
+
+    public void setFantasia(String fantasia) {
+        this.fantasia = fantasia;
+    }
+
+    public String getLongradouro() {
+        return longradouro;
+    }
+
+    public void setLongradouro(String longradouro) {
+        this.longradouro = longradouro;
+    }
+
+    public int getNro() {
+        return nro;
+    }
+
+    public void setNro(int nro) {
+        this.nro = nro;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getMunicipio() {
+        return municipio;
+    }
+
+    public void setMunicipio(String municipio) {
+        this.municipio = municipio;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getFone() {
+        return fone;
+    }
+
+    public void setFone(String fone) {
+        this.fone = fone;
+    }
+
+    public String getIe() {
+        return ie;
+    }
+
+    public void setIe(String ie) {
+        this.ie = ie;
+    }
+
+    
+}
